ebb4f1456cdb1e6e6d7dbf7140aa6029020faeec
galihnugraha <61906823+galihnugraha@users.noreply.github.com>
2020-12-04 22:23:16 +0700

    update navbar (galih - backend)

--- testimoni.php
+++ testimoni.php
@@ -1,9 +1,96 @@
+<?php 
+session_start();
+
+  require 'functions.php';
+
+  // code signup
+  if (isset($_POST['submitsignup'])) {
+    if (isset($_POST['namasignup']) && !empty($_POST['namasignup']) && isset($_POST['emailsignup']) && !empty($_POST['emailsignup']) && isset($_POST['passwordsignup']) && !empty($_POST['passwordsignup'])) {
+      $e_signup = $_POST["emailsignup"];
+
+      if (registrasi($_POST) > 0) {
+
+        //set session
+        $_SESSION["login"] = true;
+        $_SESSION["user"] = $e_signup;
+
+        echo "
+          <script>
+            alert('registrasi berhasil')
+            document.location.href = 'index.php'
+          </script>
+        ";
+      } else {
+        echo mysqli_error($conn);
+      }
+    } else {
+      echo "<script>alert('Isi data dengan lengkap!')</script>";
+    }
+  }
+  // akhir code signup
+
+  // code login
+  if (isset($_POST['submitlogin'])) {
+    $e_login = $_POST['emaillogin'];
+    $p_login = $_POST['passwordlogin'];
+    $result = mysqli_query($conn, "SELECT * FROM user WHERE email = '$e_login'");
+
+    // cek username
+    if (mysqli_num_rows($result) === 1) {
+      $row = mysqli_fetch_assoc($result);
+
+      // cek password admin
+      if ($row['email'] == 'admin') {
+        if ($p_login == 'admin') {
+
+          //set session
+          $_SESSION["admin"] = true;
+          $_SESSION["login"] = true;
+          $_SESSION["user"] = $e_login;
+
+          header("Location: index.php");
+          exit;
+        } else {
+          echo "<script>alert('Password admin salah')</script>";
+        }
+      } else {
+        // cek password user selain admin
+        if (password_verify($p_login, $row["password"])) {
+          
+          //set session
+          $_SESSION["login"] = true;
+          $_SESSION["user"] = $e_login;
+
+          header("Location: index.php");
+          exit;
+        } else {
+          echo "<script>alert('email: $e_login | Password anda salah')</script>";
+        }
+      }
+    } else {
+      echo "<script>alert('email: $e_login tidak ditemukan')</script>"; 
+    }
+  }
+  // akhir code login
+
+  // code untuk menampilkan akun di kanan navbar
+  if (isset($_SESSION["user"])) {
+    $e_logout = $_SESSION["user"];
+
+    $user = query("SELECT * FROM user WHERE email = '$e_logout'")[0];
+  }
+  // akhir code untuk menampilkan akun
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
-	<title>E-course ITC</title>
+	<link rel="icon" href="image/logo-itc.jpg">
+	<title>Testimonial</title>
+
 	<link rel="stylesheet" type="text/css" href="assets/bootstrap4/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="style2.css">
 	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 	<script type="text/javascript" src="assets/bootstrap4/js/bootstrap.min.js"></script>
@@ -12,21 +99,9 @@
 <body>
 	<!-- Navbar -->
 	<nav class="navbar navbar-expand-lg navbar-light">
-		
-		<!-- Pop up samping -->
-		<div class="menu">
-			<a href="#overlay">
-				<img src="image/menu.png" alt="menu">
-			</a>
-		</div>
 
-		<div class="overlay" id="overlay">
-			<div class="close">
-				<a href="#">X</a>
-			</div>
-		</div>
-		
-		<a class="navbar-brand" href="index.php"  style="font-size: 30px; margin-left: 10px; font-weight: bold;">ITC</a>
+    <a href="index.php"><img src="image/logo-itc.jpg" height="45" style="margin-left: 50px; margin-right: 10px;"></a>
+		<a class="navbar-brand" href="index.php"  style="font-size: 30px; font-weight: bold;">ITC</a>
 
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
@@ -36,131 +111,117 @@
 			<ul class="navbar-nav mr-auto">
 
 				<li class="nav-item">
-					<a class="nav-link text-dark" href="index.php"><b>HOME</b></a>
+					<a class="nav-link" href="about.php" style="margin-left: 30px; margin-right: 15px;">About Us</a>
 				</li>
 
-				<li class="nav-item">
-					<a class="nav-link text-dark" href="about.php">About</a>
-				</li>
+        <li class="nav-item">
+          <a class="nav-link" href="testimoni.php" style="margin-right: 15px;">Testimonial</a>
+        </li>
 
-				<li class="nav-item dropdown">
-					<a class="nav-link dropdown-toggle" href="#" id="ourProduct" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-						Our Product
-					</a>
-					<div class="dropdown-menu" aria-labelledby="ourProduct">
-						<a class="dropdown-item" href="">Platinum</a>
-						<a class="dropdown-item" href="">Gold</a>
-						<a class="dropdown-item" href="">Reguler</a>
-					</div>
-				</li>
-				
-				<li class="nav-item dropdown">
-					<a class="nav-link dropdown-toggle" href="#" id="Material" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-						Our Product
-					</a>
-					<div class="dropdown-menu" aria-labelledby="Material">
-						<a class="dropdown-item" href="">SD</a>
-						<a class="dropdown-item" href="">SMP</a>
-						<a class="dropdown-item" href="">SMA</a>
-					</div>
-				</li>
+        <?php if (isset($_SESSION["login"])) : ?>
+        <li class="nav-item dropdown">
+          <a class="nav-link nav-konten" href="login.php" style="margin-right: 15px;">
+            <div class="nav-konten-text">Content</div>
+            <div class="nav-konten-logo"></div>
+          </a>
+          <div class="dropdown-content">
+            <a href="login.php#content-1">Learn HTML</a>
+            <a href="login.php#content-1">Learn CSS</a>
+            <a href="login.php#content-2">Learn Javascript</a>
+            <a href="login.php#content-2">Learn PHP</a>
+            <a href="login.php#content-3">Learn SQL</a>
+          </div>
+        </li>
+        <?php endif; ?>
 
-				<li class="nav-item dropdown">
-					<a class="nav-link dropdown-toggle" href="#" id="Exam" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-						Exam
-					</a>
-					<div class="dropdown-menu" aria-labelledby="Exam">
-						<a class="dropdown-item" href="">UTBK</a>
-						<a class="dropdown-item" href="">UN</a>
-					</div>
-				</li>
-
-				<li class="nav-item">
-					<a class="nav-link" href="#">Live</a>
-				</li>
+        <?php if (isset($_SESSION["admin"])) : ?>
+        <li class="nav-item">
+          <a class="nav-link" href="admin">Admin Page</a>
+        </li>
+        <?php endif; ?>
 
 			</ul>
 
+    <?php if (!isset($_SESSION["login"])) : ?>
 		<div class="inline">
-
-			<div class="row">
-				<div class="col-md-6">
-				<a href="#" data-toggle="modal" data-target="#mylogin" class="btn btn-success"><span class="">Login</a>
-				</div>
-				<div class="col-md-6">
-				<a href="#" data-toggle="modal" data-target="#mydaftar" class="btn btn-success "><span class="">SignUp</a>
-				</div>
-			</div>
-
+				<li data-toggle="modal" data-target="#mylogin"><a href="#"><span class="border-right p-2">Login</a>
+				<li data-toggle="modal" data-target="#mydaftar"><a href="#"><span class="p-2">Sign Up</a>
 		</div>
+    <?php endif; ?>
 
+    <?php if (isset($_SESSION["login"])) : ?>
+    <div class="inline">
+      <p>Login as: <?= $user["nama"];?> / <?= $user["email"];?> 
+      <b style="color: white; font-size: 25px;">|</b>
+      <a href="logout.php">Logout</a> </p>
+    </div>
+    <?php endif; ?>
 		
 		</div>
 	</nav>
+  <!-- End Navbar -->
 
-				<!-- Pop up daftar -->
-				<div id="mydaftar" class="modal fade" role="dialog">
-				  <div class="modal-dialog">
+  <!-- Pop up daftar -->
+  <div id="mydaftar" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+      <!-- Modal content-->
+    <div class="modal-content">
+        <div class="modal-header">
+            <h4 class="modal-title">Sign Up</h4>
+          </div>
+  <form action="" method="post">
+    <div class="modal-body">
+    <div class="form-group">
+        <label for="fullnameSignup">Fullname</label>
+        <input type="text" name="namasignup" class="form-control" id="fullnameSignup" placeholder="Fullname" autocomplete="off" autofocus="">
+      </div>
+      <div class="form-group">
+        <label for="exampleInputEmail1">Email address</label>
+        <input type="email" name="emailsignup" class="form-control" id="exampleInputEmail1" placeholder="Email" autocomplete="off">
+      </div>
+      <div class="form-group">
+        <label for="exampleInputPassword1">Password</label>
+        <input type="password" name="passwordsignup" class="form-control" id="exampleInputPassword1" placeholder="Password">
+      </div>
+    </div>
+      <div class="modal-footer">
+        <button type="submit" name="submitsignup" class="btn btn-success">Daftar</button>
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+  </form>
+    </div>
+    </div>
+  </div>
+  <!-- End Pop up daftar -->
 
-				    <!-- Modal content-->
-				    <div class="modal-content">
-				      <div class="modal-header">
-				        <h4 class="modal-title">Sign Up</h4>
-				      </div>
-				      <div class="modal-body">
-				        <form>
-				     <div class="form-group">
-				      <label for="exampleInputEmail1">Fullname</label>
-				      <input type="text" class="form-control" id="Fullname" placeholder="Fullname">
-				    </div>
-				    <div class="form-group">
-				      <label for="exampleInputEmail1">Email address</label>
-				      <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
-				    </div>
-				    <div class="form-group">
-				      <label for="exampleInputPassword1">Password</label>
-				      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-				    </div>
-				  </form>
-				      </div>
-				      <div class="modal-footer">
-				       <button type="button" class="btn btn-success" data-dismiss="modal">Daftar</button>
-				        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-				      </div>
-				    </div>
-				  </div>
-				</div>
-				<!-- Pop up daftar -->
-
-				<!-- Pop up login -->
-				<div id="mylogin" class="modal fade" role="dialog">
-				  <div class="modal-dialog">
-
-				    <!-- Modal content-->
-				    <div class="modal-content">
-				      <div class="modal-header">
-				        <h4 class="modal-title">Login</h4>
-				      </div>
-				      <div class="modal-body">
-				        <form>
-				    <div class="form-group">
-				      <label for="exampleInputEmail1">Username</label>
-				      <input type="text" class="form-control" id="username" placeholder="Username">
-				    </div>
-				    <div class="form-group">
-				      <label for="exampleInputPassword1">Password</label>
-				      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-				    </div>
-				  </form>
-				      </div>
-				      <div class="modal-footer">
-				       <button type="button" class="btn btn-success" data-dismiss="modal">Login</button>
-				        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-				      </div>
-				    </div>
-				  </div>
-				</div>
-				<!-- End Pop up Login -->	
+  <!-- Pop up login -->
+  <div id="mylogin" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <h4 class="modal-title">Login</h4>
+        </div>
+  <form action="" method="post">      
+    <div class="modal-body">
+        <div class="form-group">
+          <label for="emailLogin">Email</label>
+          <input type="text" name="emaillogin" class="form-control" id="emailLogin" placeholder="Email" autocomplete="off" required="" autofocus="">
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword2">Password</label>
+          <input type="password" name="passwordlogin" class="form-control" id="exampleInputPassword2" placeholder="Password" required="">
+        </div>
+      </div>
+        <div class="modal-footer">
+         <button type="submit" name="submitlogin" class="btn btn-success" >Login</button>
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+  </form>
+      </div>
+    </div>
+  </div>
+  <!-- End Pop up Login --> 	
 			
 
 		<div class="bg-white ">
@@ -320,10 +381,6 @@
 			</div>			
 		</div>
 
-
-		
-
- 
         	
      <!-- Footer -->
 <footer class="page-footer font-small blue-grey lighten-5 bg-warning">
@@ -394,42 +451,42 @@
       <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
 
         <!-- Links -->
-        <h6 class="text-uppercase font-weight-bold">ABOUT US</h6>
+        <h6 class="text-uppercase font-weight-bold">Menu</h6>
         <hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
         <p>
-          <a class="dark-grey-text" href="about.php">About</a>
+          <a class="dark-grey-text" href="index.php">Home</a>
         </p>
         <p>
-          <a class="dark-grey-text" href="textimoni.php">Testimonial</a>
+          <a class="dark-grey-text" href="about.php">About Us</a>
         </p>
+      <?php if (isset($_SESSION["login"])) : ?>
         <p>
-          <a class="dark-grey-text" href="#!"></a>
+          <a class="dark-grey-text" href="login.php">Content</a>
         </p>
+      <?php endif; ?>
+      <?php if (isset($_SESSION["admin"])) : ?>
         <p>
-          <a class="dark-grey-text" href="#!"></a>
+          <a class="dark-grey-text" href="admin">Admin Page</a>
         </p>
+      <?php endif; ?>
 
       </div>
       <!-- Grid column -->
 
       <!-- Grid column -->
-      <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+      <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
 
         <!-- Links -->
-        <h6 class="text-uppercase font-weight-bold">Useful links</h6>
+        <h6 class="text-uppercase font-weight-bold">Content</h6>
         <hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
-        <p>
-          <a class="dark-grey-text" href="#!">Your Account</a>
-        </p>
-        <p>
-          <a class="dark-grey-text" href="#!">Become an Affiliate</a>
-        </p>
-        <p>
-          <a class="dark-grey-text" href="#!">Shipping Rates</a>
-        </p>
-        <p>
-          <a class="dark-grey-text" href="#!">Help</a>
-        </p>
+
+        <p class="dark-grey-text">Learn HTML</p>
+        
+        <p class="dark-grey-text">Learn CSS</p>
+        
+        <p class="dark-grey-text">Learn Javascript</p>
+
+        <p class="dark-grey-text">Learn PHP and SQL</p>
 
       </div>
       <!-- Grid column -->
